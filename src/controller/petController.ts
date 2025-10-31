aef2e3de5f411dc219d589ccd8b075c7c9b71a0f
Riobiss <riobisofc@gmail.com>
2025-10-31 17:09:32 -0300

    added TypePet

--- src/controller/petController.ts
+++ src/controller/petController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express"
+import type TypePet from "../types/TypePet"
 
-let listOfPets = []
+let listOfPets: TypePet[] = []
 
 export default class PetController {
   createPet(req: Request, res: Response) {
-    const newPet = req.body
+    const { id, name, age, species, adopted } = req.body as TypePet
+    const newPet: TypePet = { id, name, age, species, adopted }
     listOfPets.push(newPet)
     return res.status(201).json(newPet)
   }
---
+++ src/types/TypePet.ts
@@ -0,0 +1,8 @@
+type TypePet = {
+  id: number
+  name: string
+  age: number
+  species: string
+  adopted: boolean
+}
+export default TypePet
