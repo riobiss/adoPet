60de23efe38518ca1e38ee8bc03d75fd8d0e4ca1
Riobiss <riobisofc@gmail.com>
2025-11-07 07:59:51 -0300

    add AdopterRouter

---
+++ src/routes/AdopterRouter.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import AdopterRepository from "../repositories/AdopterRepository"
+import AdopterController from "../controller/AdopterController"
+import { AppDataSource } from "../config/dataSource"
+const router = express.Router()
+
+const adopterRepository = new AdopterRepository(
+  AppDataSource.getRepository("AdopterEntity")
+)
+const adopterController = new AdopterController(adopterRepository)
+router.post("/", (req, res) => adopterController.createAdopter(req, res))
+
+export default router
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import petRouter from "../routes/PetRouter"
+import adopterRouter from "../routes/AdopterRouter"
 const router = (app: express.Router) => {
  app.use("/pets", petRouter)
+ app.use("/adopter", adopterRouter)
 }
 
 export default router
