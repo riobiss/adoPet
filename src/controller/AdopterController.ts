2d1805dba7bf22e591d5506ea9835ad6c37660c8
Riobiss <riobisofc@gmail.com>
2025-11-07 07:57:58 -0300

    add AdopterController

---
+++ src/controller/AdopterController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express"
+import AdopterEntity from "../entities/AdopterEntities"
+import AdopterRepository from "../repositories/AdopterRepository"
+
+export default class AdopterController {
+  constructor(private AdopterRepo: AdopterRepository) {}
+  async createAdopter(req: Request, res: Response) {
+    const { name, password, phone, address, photo } = <AdopterEntity>req.body
+    const newAdopter = new AdopterEntity(name, password, phone, address, photo)
+    await this.AdopterRepo.createAdopter(newAdopter)
+    return res.status(200).json({ message: "Adopter Created" })
+  }
+}
