696d66164298ecfb62c698a41f65886864a52cb0
Riobis <riobisofc@gmail.com>
2025-11-18 17:20:15 -0300

    implement types in controllers, pet and adopter

--- src/controller/AdopterController.ts
+++ src/controller/AdopterController.ts
@@ -2,53 +2,76 @@ import { Request, Response } from "express"
 import AdopterEntity from "../entities/AdopterEntities"
 import AdopterRepository from "../repositories/AdopterRepository"
 import AddressEntity from "../entities/AddressEntities"
+import {
+  TypeRequestBodyAdopter,
+  TypeRequestParamsAdopter,
+  TypeResponseBodyAdopter,
+} from "../types/typesAdopter"
 
 export default class AdopterController {
   constructor(private AdopterRepo: AdopterRepository) {}
-  async createAdopter(req: Request, res: Response) {
+  async createAdopter(
+    req: Request<{}, {}, TypeRequestBodyAdopter>,
+    res: Response<TypeResponseBodyAdopter>
+  ) {
     const { name, password, phone, address, photo } = <AdopterEntity>req.body
     const newAdopter = new AdopterEntity(name, password, phone, address, photo)
     await this.AdopterRepo.createAdopter(newAdopter)
-    return res.status(200).json({ message: "Adopter Created" })
+    return res.status(201).json({ data: { id: newAdopter.id, name, phone } })
   }
-  async listAdopter(req: Request, res: Response) {
+  async listAdopter(
+    req: Request<TypeRequestParamsAdopter, {}, TypeRequestBodyAdopter>,
+    res: Response<TypeResponseBodyAdopter>
+  ) {
     const listOfAdopter = await this.AdopterRepo.listAdopter()
-    return res.status(200).json(listOfAdopter)
+    const data = listOfAdopter.map(adopter => {
+      return { id: adopter.id, name: adopter.name, phone: adopter.phone }
+    })
+    return res.json({ data })
   }
-  async updateAdopter(req: Request, res: Response) {
+  async updateAdopter(
+    req: Request<TypeRequestParamsAdopter, {}, TypeRequestBodyAdopter>,
+    res: Response<TypeResponseBodyAdopter>
+  ) {
     const { id } = req.params
     const { success, message } = await this.AdopterRepo.updateAdopter(
       Number(id),
       req.body as AdopterEntity
     )
     if (!success) {
-      return res.status(404).json({ message })
+      return res.status(404).json({ error: message })
     }
-    return res.status(200).json({ message })
+    return res.status(204)
   }
-  async deleteAdopter(req: Request, res: Response) {
+  async deleteAdopter(
+    req: Request<TypeRequestParamsAdopter, {}, TypeRequestBodyAdopter>,
+    res: Response<TypeResponseBodyAdopter>
+  ) {
     const { id } = req.params
     const { success, message } = await this.AdopterRepo.deleteAdopter(
       Number(id)
     )
     if (!success) {
-      return res.status(404).json({ message })
+      return res.status(404).json({ error: message })
     }
     if (success) {
-      return res.status(200).json({ message })
+      return res.status(204)
     }
   }
-  async updateAddressAdopter(req: Request, res: Response) {
+  async updateAddressAdopter(
+    req: Request<TypeRequestParamsAdopter, {}, TypeRequestBodyAdopter>,
+    res: Response<TypeResponseBodyAdopter>
+  ) {
     const { id } = req.params
     const { success, message } = await this.AdopterRepo.updateAddressAdopter(
       Number(id),
-      req.body as AddressEntity
+      req.body.address as AddressEntity
     )
     if (!success) {
-      return res.status(404).json({ message })
+      return res.status(404).json({ error: message })
     }
     if (success) {
-      return res.status(200).json({ message })
+      return res.status(204)
     }
   }
 }
--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -3,55 +3,85 @@ import EnumSpecies from "../enum/EnumSpecies"
 import PetRepository from "../repositories/PetRepository"
 import PetEntity from "../entities/PetEntities"
 import EnumSize from "../enum/EnumSize"
+import {
+  TypeRequestBodyPet,
+  TypeRequestParamsPet,
+  TypeResponseBodyPet,
+} from "../types/typesPet"
 
 export default class PetController {
   constructor(private petRepo: PetRepository) {}
-  async createPet(req: Request, res: Response) {
+  async createPet(
+    req: Request<TypeRequestParamsPet, {}, TypeRequestBodyPet>,
+    res: Response<TypeResponseBodyPet>
+  ) {
     const { name, species, size, dateOfBirth, adopted } = <PetEntity>req.body
     if (!Object.values(EnumSpecies).includes(species)) {
-      return res.status(400).json({ erro: "Species invalid" })
+      return res.status(400).json({ error: "Species invalid" })
     }
     if (size && !(size in EnumSize)) {
-      return res.status(400).json({ erro: "Size invalid" })
+      return res.status(400).json({ error: "Size invalid" })
     }
 
     const newPet = new PetEntity(name, species, dateOfBirth, adopted, size)
     await this.petRepo.createPet(newPet)
-    return res.status(201).json(newPet)
+    return res
+      .status(201)
+      .json({ data: { id: newPet.id, name, species, size } })
   }
-  async listPets(req: Request, res: Response) {
+  async listPets(
+    req: Request<TypeRequestParamsPet, {}, TypeRequestBodyPet>,
+    res: Response<TypeResponseBodyPet>
+  ) {
     const listOfPets = await this.petRepo.listPets()
-    return res.status(200).json(listOfPets)
+    const data = listOfPets.map(pet => {
+      return {
+        id: pet.id,
+        name: pet.name,
+        species: pet.species,
+        size: pet.size,
+      }
+    })
+    return res.status(200).json({ data })
   }
-  async updatePet(req: Request, res: Response) {
+  async updatePet(
+    req: Request<TypeRequestParamsPet, {}, TypeRequestBodyPet>,
+    res: Response<TypeResponseBodyPet>
+  ) {
     const { id } = req.params
     const { success, message } = await this.petRepo.updatePet(
       Number(id),
       req.body as PetEntity
     )
     if (!success) {
-      return res.status(404).json({ message })
+      return res.status(404).json({ error: message })
     }
-    return res.status(200).json({ message })
+    return res.status(204)
   }
-  async deletePet(req: Request, res: Response) {
+  async deletePet(
+    req: Request<TypeRequestParamsPet, {}, TypeRequestBodyPet>,
+    res: Response<TypeResponseBodyPet>
+  ) {
     const { id } = req.params
     const { success, message } = await this.petRepo.deletePet(Number(id))
     if (!success) {
-      return res.status(404).json({ erro: message })
+      return res.status(404).json({ error: message })
     }
-    return res.status(200).json({ message })
+    return res.status(204)
   }
-  async adoptPet(req: Request, res: Response) {
+  async adoptPet(
+    req: Request<TypeRequestParamsPet, {}, TypeRequestBodyPet>,
+    res: Response<TypeResponseBodyPet>
+  ) {
     const { pet_id, adopter_id } = req.params
     const { success, message } = await this.petRepo.adoptPet(
       Number(pet_id),
       Number(adopter_id)
     )
     if (!success) {
-      return res.status(404).json({ message })
+      return res.status(404).json({ error: message })
     }
-    return res.status(200).json({ message })
+    return res.status(204)
   }
   async searchPetByField(req: Request, res: Response) {
     const { field, value } = req.query
